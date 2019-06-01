43cd95027bdc0749c4fda7cc3ff1a8737934d2ea
John Jardin <john@agilite.io>
2019-06-01 04:58:34 +0200

    - Agilit-e Node Module V6.3.1

---
+++ agilite/data-templates/keywords.js
@@ -0,0 +1,17 @@
+const data = {
+    new: {
+        data: {
+            key: "",
+            values: [ { "label": "key1", "value": "value1" } ]
+        }
+    },
+    modified: {
+        data: {
+            key: "",
+            groupName: "",
+            values: [ { "label": "key11", "value": "value11" } ]
+        }
+    }
+};
+
+module.exports = data;
